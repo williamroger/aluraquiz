d4936f773384bc2fc48affa7e07bf77038780c54
William Roger <williamroger.frontend@gmail.com>
2021-01-26 00:28:41 -0300

    Adicionado db.json + componentes básicos da aplicação

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,47 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Widget from '../src/components/Widget';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import db from '../db.json';
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  } 
+`;
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Movie Quiz</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </Widget.Content>
+        </Widget>
+
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da Galera</h1>
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+
+        <GitHubCorner projectUrl="https://github.com/williamroger/aluraquiz" />
+      </QuizContainer>
+    </QuizBackground>
+  );
 }
