a71615478cf0673ef6203c33bdfe664516fd6ed5
William Roger <williamroger.frontend@gmail.com>
2021-01-28 23:11:57 -0300

    Finalizado Estrutura e Estilo página Quiz

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,14 @@ import styled from 'styled-components';
 import { useRouter } from 'next/router';
 
 import db from '../db.json';
-import Widget from '../src/components/Widget';
-import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+import QuizLogo from '../src/components/QuizLogo';
+import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
-
-const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  } 
-`;
 
 export default function Home() {
   const router = useRouter();
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,9 +1,55 @@
 import React from 'react';
 
+import db from '../db.json';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+import QuizLogo from '../src/components/QuizLogo';
+import Widget from '../src/components/Widget';
+import Button from '../src/components/Button';
+
+function LoadWidget() {
+  return (
+    <Widget>
+      <Widget.Header>
+        Carregando...
+      </Widget.Header>
+
+      <Widget.Content>
+        [Desafio do Loading]
+      </Widget.Content>
+    </Widget>
+  );
+}
+
 export default function QuizPage() {
   return (
-    <div>
-      <h1>Página do principal do Quiz!</h1>
-    </div>
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>Pergunta 1 de 20</h1>
+          </Widget.Header>
+          <img 
+            style={{
+              width: '100%',
+              height: '150px',
+              objectFit: 'cover',
+            }}
+            src="https://placehold.it/400x400" 
+            alt="Imagem"
+          />
+          <Widget.Content>
+            <h2>Título</h2>
+            <p>Descrição</p>
+            <Button>
+              Confirmar
+            </Button>
+          </Widget.Content>
+        </Widget>
+        
+        <LoadWidget />
+      </QuizContainer>
+    </QuizBackground>
   );
 }
